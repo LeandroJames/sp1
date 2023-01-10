ae1810d8ca8245e7cbbe226b173998bc73bdc284
LeandroJames <leandroespanol@gmail.com>
2023-01-10 13:32:27 +0100

    This is another attempt at commitment

--- arrow_functions_and_classes.js
+++ arrow_functions_and_classes.js
@@ -5,10 +5,10 @@ console.log(addNumbers(5, 7));
 //Crea una arrow function que, rebent un paràmetre, retorni un objecte amb un atribut que tingui com a valor el paràmetre rebut.
 
 const setCarBrand = (brand) => {
-    let car = {brand: brand}
-    return car
-} 
-console.log(setCarBrand("Seat"))
+  let car = { brand: brand };
+  return car;
+};
+console.log(setCarBrand("Seat"));
 
 //Crea una classe "Persona" que rebi un paràmetre 'nom' en ser instanciada. La classe inclourà un mètode dirNom que imprimeixi per consola el paràmetre 'nom'. Invoca el mètode dirNom des de fora de la classe.
 class Person {
@@ -23,3 +23,17 @@ const name = "Wild Bill";
 const person1 = new Person(name);
 console.log(person1.sayMyName());
 
+//Escriu una function creadora d'objectes que faci instàncies d'una classe abstracta. Invoca-la amb diferents definicions.
+class PersonalDetails {
+  #address;
+  #email;
+  #bank_account_number;
+  constructor() {
+    if (this.constructor == PersonalDetails) {
+      throw Error(
+        "This information is private and you, my dear, are allowed no access"
+      );
+    }
+  }
+}
+console.log(new PersonalDetails());
