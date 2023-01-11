52b19c37d66177b103b0d2abae6e57c66ec92840
LeandroJames <116574668+LeandroJames@users.noreply.github.com>
2023-01-11 16:17:30 +0100

    Delete try_catch.js

--- try_catch.js
+++
@@ -1,4 +0,0 @@
-const myPromise = new Promise((resolve, reject) =>
-  false ? resolve(5) : reject(new Error("HERROR"))
-);
-myPromise.then((value) => console.log(value)).catch((error)=>console.error(error.message));
